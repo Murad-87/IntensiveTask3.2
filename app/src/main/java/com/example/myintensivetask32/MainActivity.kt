ee9102bfca4422a44951341b2560ac8b7f6219d9
unknown <89380041414@gmail.com>
2022-04-11 13:25:00 +0300

    Реализовано открытие картинки по ссылке

--- app/src/main/java/com/example/myintensivetask32/MainActivity.kt
+++ app/src/main/java/com/example/myintensivetask32/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.myintensivetask32
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.URLUtil
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.bumptech.glide.Glide
+import com.example.myintensivetask32.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        showPicture()
+
     }
+
+    private fun showPicture() {
+        val glide = Glide.with(this)
+        with(binding) {
+            editT.addTextChangedListener { url ->
+                if (isUrlValid(url.toString())) {
+                    glide.load(url.toString())
+                        .error(R.drawable.ic_error)
+                        .centerCrop()
+                        .into(imageView)
+                } else {
+                    Toast.makeText(this@MainActivity, "Ошибка", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun isUrlValid(url: String) = URLUtil.isValidUrl(url)
+
 }
+
+
+
+
